63ff608025e7e94e1c1e04c00dc8f2045a56ba47
AndraTodor <andra_t27@yahoo.com>
2024-07-14 22:08:26 +0300

    fara poze

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,6 +4,29 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'x-api-key': 'al  tau',
+    'x-api-key':
+      'live_BbC2EF5rJUMSGRwCoPmF804zBd1v7xCMEUth0NSNW11Zqfz0pHf6j020T9MaXWDf',
   },
 });
+
+async function fetchBreeds() {
+  try {
+    const response = await axiosInstance.get('/breeds');
+    return response.data;
+  } catch (error) {
+    throw new Error(`error: ${error.message}`);
+  }
+}
+
+async function fetchCatByBreed(breedId) {
+  try {
+    const response = await axiosInstance.get('/images/search', {
+      params: { breed_ids: breedId },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(`error: ${error.message}`);
+  }
+}
+
+export { fetchBreeds, fetchCatByBreed };
